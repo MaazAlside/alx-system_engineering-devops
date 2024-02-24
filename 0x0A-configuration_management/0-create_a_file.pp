0f7736ab3e103dfc1f94446cc8998a8a7ca6f30c
maazalside <maazalside@gmail.com>
2024-02-24 08:35:11 +0300

    fix style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,13 @@
-# 0-create_a_file.pp
+# Puppet Manifest: 0-create_a_file.pp
+# Purpose: Ensure the existence of a file in /tmp/school with specific permissions and content.
+
+# Ensure the www-data user exists
 user { 'www-data':
   ensure => present,
 }
 
+
+# Create a file in /tmp/school with specified owner, group, mode, and content
 file { '/tmp/school':
   owner => 'www-data',
   group => 'www-data',
